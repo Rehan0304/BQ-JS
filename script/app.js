5e8448256881fa60ad42f59b95465bbc60fd6d37
Muhammad rehan <109660304+Rehan0304@users.noreply.github.com>
2023-04-10 17:48:00 +0500

    adding docoument.write

--- script/app.js
+++ script/app.js
@@ -2,3 +2,4 @@ let zakatValue=2.5/100;
 // console.log(zakatValue)
 let userIncome=Number(prompt("please enter your value:"));
 console.log(zakatValue*userIncome);
+document.write(zakatValue*userIncome);
