27b598bb31148543df587c7a82a4b4757b617ad2
Muhammad rehan <109660304+Rehan0304@users.noreply.github.com>
2023-04-21 19:59:30 +0500

    FEAT:adding new feature

--- script/app.js
+++ script/app.js
@@ -1,5 +1,62 @@
-let zakatValue=2.5/100;
-// console.log(zakatValue)
-let userIncome=Number(prompt("please enter your value:"));
-console.log(zakatValue*userIncome);
-document.write(zakatValue*userIncome);
+
+// **************ZAKAT CALCULATOR***************
+
+// let zakatValue=2.5/100;
+// // console.log(zakatValue)
+// let userIncome=Number(prompt("please enter your value:"));
+// console.log(zakatValue*userIncome);
+// document.write(zakatValue*userIncome);
+
+// **********FITRAH CALCULATOR*************
+
+// let familyMembers = +prompt("Enter total number of family members");
+// // console.log(familyMembers);
+// let fitraMethod = +prompt(
+//   "please select fitra method: 1 for gandum,2 for jau,3 for khajoor,4 for kishmish"
+// );
+// // console.log(fitraMethod);
+// if (fitraMethod === 1) {
+//   alert(
+//     "your fitrah amount according to your selected method: " +
+//       familyMembers * 320
+//   );
+// } else if (fitraMethod === 2) {
+//   alert(
+//     "your fitrah amount according to your selected method: " +
+//       familyMembers * 800
+//   );
+// } else if (fitraMethod === 3) {
+//   alert(
+//     "your fitrah amount according to your selected method: " +
+//       familyMembers * 2800
+//   );
+// } else if (fitraMethod === 4) {
+//   alert(
+//     "your fitrah amount according to your selected method: " +
+//       familyMembers * 6400
+//   );
+// } else {
+//   alert("please enter correct method!");
+// }
+
+
+// *************USER GUESS GAME*************
+
+// Math.random  creates random number
+// let secertNumber= Math.floor(Math.random()*11);
+// console.log(secertNumber);
+// let userGuess=+prompt("please guess the secret num between 1 and 10");
+// console.log(userGuess);
+// if (userGuess===secertNumber) {
+//     alert("congratulations! You guessed the seceret number")
+// }else if(userGuess>secertNumber){
+//     alert("Guess number is high! try again")
+// }else if(userGuess<secertNumber){
+//     alert("Guess number is low! try again")
+// }
+
+// ******************CAPITLIZED FIRST LETTER OF STRING*************************
+
+// let userName=prompt("Please enter your name");
+// let firstCap=(userName.charAt(0)).toUpperCase() + (userName.slice(1)).toLowerCase();
+// console.log(firstCap);
